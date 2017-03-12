c70a98c5ccf4b9c8e6f83d2aaef9627014526306
R Sepp <robietoser@hotmail.com>
2017-03-12 18:25:33 +0200

    Muutus kalendri CSS-is

--- application/views/header_loggedOut.php
+++ application/views/header_loggedOut.php
@@ -14,7 +14,7 @@
       <script async type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
       <!-- Include Date Range Picker -->
       <script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+      <link rel="stylesheet" href="<?php echo base_url(); ?>media/css/kalender.css"/>
       <script async type="text/javascript" src="<?php echo base_url(); ?>media/js/kalender.js"></script>
    </head>
    <body>
