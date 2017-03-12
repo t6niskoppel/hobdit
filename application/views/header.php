abecc54e5afb3c9cca268ac32e38fe065e942d66
R Sepp <robietoser@hotmail.com>
2017-03-12 18:13:21 +0200

    Kalender teise faili

--- application/views/header.php
+++ application/views/header.php
@@ -3,12 +3,19 @@
    ?><!DOCTYPE html>
 <html lang="zxx">
    <head>
-      <title>Avaleht</title>
+      <title>Hobdit</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>media/css/bootstrap.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
       <script src="<?php echo base_url(); ?>media/js/bootstrap.js"></script>
+	  
+      <!-- Date Picker -->
+      <script async type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+      <!-- Include Date Range Picker -->
+      <script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+      <script async type="text/javascript" src="<?php echo base_url(); ?>media/js/kalender.js"></script>
    </head>
    <body>
       <nav class="navbar navbar-inverse">
@@ -32,19 +39,19 @@
                   <!-- <li><a href="#">Täpsem otsing</a></li> -->
                </ul>
                <form class="navbar-form navbar-right" role="search">
+                  <label for="search" class="sr-only">Search</label>
                   <div class="form-group input-group">
-					 <label for="search" class="sr-only">Search</label>
                      <input type="text" class="form-control" placeholder="Search..." id="search">
                      <span class="input-group-btn">
                      <button class="btn btn-default" type="button">
                      <span class="glyphicon glyphicon-search"></span>
                      </button>
-					 </span>
+                     </span>
                   </div>
                </form>
                <ul class="nav navbar-nav navbar-right">
                   <li><a href="#"><span class="glyphicon glyphicon-user"></span>My account</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
+                  <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
                </ul>
             </div>
          </div>
--- application/views/header_loggedOut.php
+++ application/views/header_loggedOut.php
@@ -9,6 +9,13 @@
       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>media/css/bootstrap.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
       <script src="<?php echo base_url(); ?>media/js/bootstrap.js"></script>
+	        
+	  <!-- Date Picker -->
+      <script async type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+      <!-- Include Date Range Picker -->
+      <script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+      <script async type="text/javascript" src="<?php echo base_url(); ?>media/js/kalender.js"></script>
    </head>
    <body>
       <nav class="navbar navbar-inverse">
@@ -22,8 +29,9 @@
                   <li><a href="<?php echo base_url(); ?>index.php/welcome/search">Refined search</a></li>
                </ul>
                <form class="navbar-form navbar-right" role="search">
+			   	  <label for="search" class="sr-only">Search</label>
                   <div class="form-group input-group">
-                     <input type="text" class="form-control" placeholder="Search..">
+                     <input type="text" class="form-control" placeholder="Search.." id="search">
                      <span class="input-group-btn">
                      <button class="btn btn-default" type="button">
                      <span class="glyphicon glyphicon-search"></span>
@@ -32,9 +40,8 @@
                   </div>
                </form>
                <ul class="nav navbar-nav navbar-right">
-                   
                    <li><a href="#"><span class="glyphicon glyphicon-list-alt"></span> Sign Up</a></li>
-                    <li><a href="#" onclick="login()";><span class="glyphicon glyphicon-log-in"></span> Login</a></li>'
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                </ul>
             </div>
       </nav>
