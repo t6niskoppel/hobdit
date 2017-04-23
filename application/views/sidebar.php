46c30b3084a68a9191a002417cb8da908645128d
R Sepp <robietoser@hotmail.com>
2017-04-23 23:12:04 +0300

    Kategooriate nupud

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,9 +1,9 @@
-<div class="col-sm-2 well">
-      <p><?php echo lang("sidebar_categories"); ?></p>
-      <div class="btn-group-vertical">
-         <a href="<?php echo base_url(); ?>index.php/welcome/category" class="btn btn-primary">Kategooria 1</a>
-         <a href="<?php echo base_url(); ?>index.php/welcome/category" class="btn btn-primary">Kategooria 2</a>
-         <a href="<?php echo base_url(); ?>index.php/welcome/category" class="btn btn-primary">Kategooria 3</a>
-      </div>
+<div class='wrapper text-center'>
+   <p><?php echo lang("sidebar_categories"); ?></p>
+   <div class="btn-group-vertical">
+      <a href="<?php echo base_url(); ?>index.php/welcome/category" class="btn btn-primary">Kategooria 1</a>
+      <a href="<?php echo base_url(); ?>index.php/welcome/category" class="btn btn-primary">Kategooria 2</a>
+      <a href="<?php echo base_url(); ?>index.php/welcome/category" class="btn btn-primary">Kategooria 3</a>
+   </div>
 </div>
 </div>
