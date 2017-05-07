f5a8219eb3856c49bd846648612ac30c8cff7ea9
R Sepp <robietoser@hotmail.com>
2017-05-07 23:29:44 +0300

    Optimeerimine

--- media/js/kkkkysimused.js
+++ media/js/kkkkysimused.js
@@ -1,18 +1,1 @@
-function loadDoc() 
-{
-	var xmlhttp;
-  if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-  } else {
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("veelKysimusi").innerHTML =
-      this.responseText;
-    }
-  };
-  xmlhttp.open("GET", "/media/txt/kkkKysimused.txt", true);
-  xmlhttp.send();
-}
- 
+function loadDoc(){var xmlhttp;if(window.XMLHttpRequest){xmlhttp=new XMLHttpRequest();}else{xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}xmlhttp.onreadystatechange=function(){if(this.readyState==4&&this.status==200){document.getElementById("veelKysimusi").innerHTML=this.responseText;}};xmlhttp.open("GET","/media/txt/kkkKysimused.txt",true);xmlhttp.send();}
